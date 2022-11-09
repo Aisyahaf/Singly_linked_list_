f5b99e8e0b4a8511d6e2d87ad8b7736b0a80fa39
Aisah Fitriani <aisah.atik.ft21@mail.umy.ac.id>
2022-11-09 17:11:40 +0700

    Create While Condition to find new node location inside list

--- Singly_linked_list/Program.cs
+++ Singly_linked_list/Program.cs
@@ -21,7 +21,7 @@ namespace Singly_linked_list
             START = null;
         }
 
-        public void addNode()//Method untuk menambahkan sebuah node kedalam 
+        public void addNode()//Method untuk menambahkan sebuah node kedalam list
         {
             int nim;
             string nm;
@@ -33,7 +33,7 @@ namespace Singly_linked_list
             nodeBaru.noMhs = nim;
             nodeBaru.nama = nm;
 
-            if (START == null || nim <= START.noMhs)
+            if (START == null || nim <= START.noMhs)// Node ditambahkan sebagai node
             {
                 if((START != null) && (nim == START.noMhs))
                 {
@@ -48,6 +48,22 @@ namespace Singly_linked_list
 
             Node previous, current;
             previous = START;
+            current = START;
+
+            while ((current != null) && (nim >= current.noMhs))
+            {
+                if (nim == current.noMhs)
+                {
+                    Console.WriteLine("\nNomer Mahasiswa sama tidak diijinkan\n");
+                    return;
+                }
+                previous = current;
+                current = current.next;
+            }
+            //Node baru akan ditempatkan diantara previous dan current
+
+            nodeBaru.next = current;
+            previous.next = nodeBaru;
         }
     }
 }
