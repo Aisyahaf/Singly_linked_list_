07155003bf7b0190ff5844f81772994bf35f4b8e
Aisah Fitriani <aisah.atik.ft21@mail.umy.ac.id>
2022-11-09 17:03:14 +0700

    Create namespace Singly_linked_list, class Node, List, & addNode

--- Singly_linked_list/Program.cs
+++ Singly_linked_list/Program.cs
@@ -20,5 +20,34 @@ namespace Singly_linked_list
         {
             START = null;
         }
+
+        public void addNode()//Method untuk menambahkan sebuah node kedalam 
+        {
+            int nim;
+            string nm;
+            Console.Write("\nMasukkan nomer Mahasiswa: ");
+            nim = Convert.ToInt32(Console.ReadLine());
+            Console.Write("\nMasukkan nama Mahasiswa: ");
+            nm = Console.ReadLine();
+            Node nodeBaru = new Node();
+            nodeBaru.noMhs = nim;
+            nodeBaru.nama = nm;
+
+            if (START == null || nim <= START.noMhs)
+            {
+                if((START != null) && (nim == START.noMhs))
+                {
+                    Console.WriteLine("\nNomer Mahasiswa sama tidak diijinkan\n");
+                    return;
+                }
+                nodeBaru.next = START;
+                START = nodeBaru;
+                return;
+            }
+            //Menemukan lokasi node baru didalam list
+
+            Node previous, current;
+            previous = START;
+        }
     }
 }
